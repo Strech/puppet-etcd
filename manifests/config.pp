78887e1bd76a626df5263aca21df100e83bbaf96
Sergey Fedorov <oni.strech@gmail.com>
2016-01-22 20:26:16 +0100

    Update config.pp

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,22 +1,28 @@
 # == Class: etcd::config
 #
 class etcd::config {
-  case $::operatingsystemmajrelease {
-    6       : {
-      file { '/etc/sysconfig/etcd.conf':
-        ensure  => 'file',
-        content => template("${module_name}/etc/etcd/etcd.conf.erb"),
-      }
-    }
-    7       : {
-      file { '/etc/etcd/': ensure => 'directory', } ->
-      file { '/etc/etcd/etcd.conf':
-        ensure  => 'file',
-        content => template("${module_name}/etc/etcd/etcd.conf.erb"),
-      }
-    }
-    default : {
-      fail('Unsupported OS.')
-    }
+  file { '/etc/etcd/': ensure => 'directory', } ->
+  file { '/etc/etcd/etcd.conf':
+    ensure  => 'file',
+    content => template("${module_name}/etc/etcd/etcd.conf.erb"),
   }
+
+  #case $::operatingsystemmajrelease {
+  #  6       : {
+  #    file { '/etc/sysconfig/etcd.conf':
+  #      ensure  => 'file',
+  #      content => template("${module_name}/etc/etcd/etcd.conf.erb"),
+  #    }
+  #  }
+  #  7       : {
+  #    file { '/etc/etcd/': ensure => 'directory', } ->
+  #    file { '/etc/etcd/etcd.conf':
+  #      ensure  => 'file',
+  #      content => template("${module_name}/etc/etcd/etcd.conf.erb"),
+  #    }
+  #  }
+  #  default : {
+  #    fail('Unsupported OS.')
+  #  }
+  #}
 }
