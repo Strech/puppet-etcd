51fc7b168cd1398c596dbf432705a7b600644850
Sergey Fedorov <oni.strech@gmail.com>
2016-01-22 21:53:13 +0100

    Update service.pp

--- manifests/service.pp
+++ manifests/service.pp
@@ -7,9 +7,5 @@ class etcd::service {
     ensure => $etcd::service_ensure,
     enable => $etcd::service_enable,
     provider => 'debian',
-    #start => '/etc/init.d/etcd start',
-    #stop => '/etc/init.d/etcd stop',
-    #status => '/etc/init.d/etcd status',
-    #pattern => '/usr/bin/etcd',
   }
 }
