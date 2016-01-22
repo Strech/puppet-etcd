5b71aeb368af8b9e104b597d980c06f5aef5ccac
Sergey Fedorov <oni.strech@gmail.com>
2016-01-22 21:25:09 +0100

    Update service.pp

--- manifests/service.pp
+++ manifests/service.pp
@@ -6,5 +6,7 @@ class etcd::service {
   service { 'etcd':
     ensure => $etcd::service_ensure,
     enable => $etcd::service_enable,
+    start => '/etc/init.d/etcd start',
+    stop => '/etc/init.d/etcd stop',
   }
 }
